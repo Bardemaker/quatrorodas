ced5730774c8de12b99a413550d1508ede4db17a
Bardemaker <hbaneto@gmail.com>
2018-02-04 06:11:09 -0200

    Publicação

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Abril</title>
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">    
+</head>
+<body>    
+
+<div class="container" style="width:1322px;">
+<div class="row">
+
+<!-- BANNER SUPERIOR -->
+<p>
+<div class="banner">
+<img src="images/banner.png" class="banner">
+</div>
+</p>
+
+<!-- MENU PRINCIPAL -->
+<div class="box_vermelho">
+<div style="float:left"><img src="images/logo_4_rodas.png" class="logo"></div>
+<div class="area_menu">
+
+<!-- BOTÃO MENU RESPONSIVO -->
+<input type="checkbox" id="bt_menu">
+<label for="bt_menu" style="float:right">&#9776;</label>
+
+<nav class="menu">
+<ul>
+<li><a href="#">Carros <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul>
+</li>
+<li><a href="#">Testes <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul></li>
+<li><a href="#">Notícias <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul></li>
+<li><a href="#">Auto-Serviço <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul></li>
+<li><a href="#">Guia de Compras <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul></li>
+<li><a href="#">Tabela FIPE <span class="glyphicon glyphicon-menu-down"></span></a>
+<ul>
+<li><a href="#">Item 1</a></li>
+<li><a href="#">Item 2</a></li>
+<li><a href="#">Item 3</a></li>
+</ul></li>
+<li><a href="#">Assine</a></li>
+<li id="campo_busca"><input class="busca" type="text" placeholder="PESQUISAR"></li>
+</ul>
+</nav>
+
+</div>
+</div>
+
+<!-- MENU MAIS ACESSADOS -->
+<div style="width:1322px;" class="menu_apoio">
+<div class="mais_acessados">
++ ACESSADOS
+</div>
+<div style="background-color:#000; color:#FFF; height:30px; margin-left:142px; padding:5px; ">
+<nav class="menu_apoio">
+<ul>
+<li><a href="#">Salão do Automóvel</a></li>
+<li><a href="#">Renegade</a></li>
+<li><a href="#">Novo Sandero</a></li>
+<li><a href="#">Novo Fox</a></li>
+<li><a href="#">Novo Ka</a></li>
+<li><a href="#">HB 20</a></li>
+<li><a href="#">Duster</a></li>
+<li><a href="#">Golf</a></li>
+<li><a href="#">Corolla</a></li>
+<li><a href="#">Civic</a></li>
+<li><a href="#">| A - Z |</a></li>
+
+</ul>
+</nav>
+
+
+
+</div>
+</div>
+
+<!-- GRID PRINCIPAL -->
+
+        <div class="principal">
+        <a href="#"><img class="principal" src="images/principal.png"></a>
+        </div>
+        <div class="secundario">
+                <a href="#"><img class="secundario" src="images/secundario1.png"></a>
+
+        </div>
+         <div class="secundario">
+                <a href="#"><img class="secundario" src="images/secundario2.png"></a>
+
+        </div>
+        <div class="secundario">
+                <a href="#"><img class="secundario" src="images/secundario3.png"></a>
+
+        </div>
+       <div class="secundario">
+                <a href="#"><img class="secundario" src="images/secundario4.png"></a>
+
+        </div>
+  </div>
+   
+  
+  <p>
+  
+<!-- GRID INFERIOR -->
+
+<div class="row">
+    <div class="sub_item" style="margin-right:8px;">
+    <img src="images/sub_menu1.png" class="sub_item" >
+    </div>
+    
+    <div class="sub_item" style="margin-right:8px;">
+    <img src="images/sub_menu2.png" class="sub_item" >
+    </div>
+    
+    <div class="sub_item" style="margin-right:8px;">
+    <img src="images/sub_menu3.png" class="sub_item" >
+    </div>
+    
+    <div class="sub_item">
+    <img src="images/sub_menu4.png" class="sub_item" >
+    </div>
+    
+    </div>
+   </p> 
+    
+ </div>
+
+
+
+
+<?php
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
